fc1413d5c840f95f60e914ea7a21c3e02e45178a
Maksym MATIUSHCHENKO <mmatiush@e1r5p17.unit.ua>
2019-09-03 21:52:43 +0300

    add tableviews

--- swiftyCompanion/ProfileViewController.swift
+++ swiftyCompanion/ProfileViewController.swift
@@ -15,6 +15,8 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var skillsTable: UITableView!
+    @IBOutlet weak var projectsTable: UITableView!
     
     var json: JSON?
 
@@ -26,7 +28,8 @@ class ProfileViewController: UIViewController {
         avatar.layer.borderColor = UIColor.white.cgColor
         avatar.layer.cornerRadius = avatar.frame.width / 2
         
-        backgroundView.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        skillsTable.layer.cornerRadius = 5
+        projectsTable.layer.cornerRadius = 5
 
     }
     
@@ -36,6 +39,26 @@ class ProfileViewController: UIViewController {
 
     func setPhoto() {
         
+    }
+    
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == skillsTable, let cell = tableView.dequeueReusableCell(withIdentifier: "skillsCell", for: indexPath) as? SkillsTableViewCell {
+            cell.skillLabel.text = "Algorithm"
+            return cell
+        } else if tableView == projectsTable, let cell = tableView.dequeueReusableCell(withIdentifier: "projectsCell", for: indexPath) as? ProjectsTableViewCell {
+            cell.projectLabel.text = "Day 01 - 20.0%"
+            return cell
+        }
+        return UITableViewCell()
     }
     
 }
---
+++ swiftyCompanion/TableViewCells.swift
@@ -0,0 +1,20 @@
+//
+//  SkillsTableViewCell.swift
+//  swiftyCompanion
+//
+//  Created by Maksym MATIUSHCHENKO on 8/28/19.
+//  Copyright © 2019 Maksym MATIUSHCHENKO. All rights reserved.
+//
+
+import UIKit
+
+class SkillsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var skillLabel: UILabel!
+
+}
+
+class ProjectsTableViewCell: UITableViewCell {
+    @IBOutlet weak var projectLabel: UILabel!
+    
+}
